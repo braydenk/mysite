9dd8fc15bcda3f374a7afdb3ed119d635e2590d8
Brayden Killeen <braydenmkilleen@gmail.com>
2020-02-16 07:18:04 +1100

    Styled home page

--- src/components/header.js
+++ src/components/header.js
@@ -3,35 +3,19 @@ import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
 
-const HeaderTitle = styled.h1`
-  margin: 0;
-`
-
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <HeaderTitle>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  <header>
+    <div>
+      <h2>
+        <Link to="/" style={{ color: "#fff" }}>
+          BK.
         </Link>
-      </HeaderTitle>
+      </h2>
+      <h2>
+        <Link to="/" style={{ color: "#000" }}>
+          Menu.
+        </Link>
+      </h2>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,46 @@
 import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 50%;
+  background: #0d2f81;
+  color: #fff;
+`
+
+const TextContainer = styled.div`
+  margin: 16px;
+  margin-top: 200px;
+`
+
+const MoreLink = styled(Link)`
+  color: #fff;
+  text-decoration: underline;
+`
+
+const Navbar = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: 16px;
+  display: flex;
+  justify-content: space-between;
+`
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <Container>
+      <TextContainer>
+        <h1>Hey, I'm Brayden!</h1>
+        <h2>I'm a graduate software engineer from Melbourne, Australia.</h2>
+        <h2 style={{ paddingTop: "16px" }}>
+          <MoreLink to="/">More about me.</MoreLink>
+        </h2>
+      </TextContainer>
+    </Container>
   </Layout>
 )
 
